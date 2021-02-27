aa3edea2a092faaed72a44b8443f7db71b946b5a
zodac <arouge110@msn.com>
2021-02-27 17:36:33 +1300

    Minor change to verify laptop setup

--- screenshot-service-war/src/main/java/me/zodac/screenshot/rest/JaxRsActivator.java
+++ screenshot-service-war/src/main/java/me/zodac/screenshot/rest/JaxRsActivator.java
@@ -7,7 +7,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /**
- * A class extending {@link Application} and annotated with @ApplicationPath is the Java EE 6 "no XML" approach to activating JAX-RS.
+ * A class extending {@link Application} and annotated with {@link ApplicationPath} is the Java EE 6 "no XML" approach to activating JAX-RS.
  * <p>
  * Resources are served relative to the servlet path specified in the {@link ApplicationPath} annotation.
  */
@@ -16,7 +16,7 @@ public class JaxRsActivator extends Application {
 
     @Override
     public Set<Class<?>> getClasses() {
-        final Set<Class<?>> classes = new HashSet<>();
+        final Set<Class<?>> classes = new HashSet<>(1);
         classes.add(ScreenshotEndpoint.class);
         return classes;
     }
